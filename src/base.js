234be142457a22c855e7d4f8b12212de9bef9e4a
fenriswolf <alex0014@gmail.com>
2014-08-04 18:32:17 +0800

    Remove google analytics.

--- src/base.js
+++ src/base.js
@@ -555,24 +555,5 @@ function init() {
 
 $(document).ready(init);
 
-/* google analytics */
 var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-11830182-16']);
-_gaq.push(['_setCustomVar',
-    1,                // This custom var is set to slot #1.  Required parameter.
-    'Version',    // The name of the custom variable.  Required parameter.
-    sense.VERSION,        // The value of the custom variable.  Required parameter.
-    1                 // Sets the scope to visitor-level.  Optional parameter.
-]);
-
 _gaq.push(['_trackPageview']);
-
-(function () {
-    var ga = document.createElement('script');
-    ga.type = 'text/javascript';
-    ga.async = true;
-    ga.src = 'https://ssl.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(ga, s);
-})();
-
